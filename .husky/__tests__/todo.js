3fbe9f605bd18630cb24c8a6fbfb5bac8423aaad
meghanahoney <113693788+meghanahoney@users.noreply.github.com>
2022-11-18 21:00:27 +0530

    Add files via upload

---
+++ .husky/__tests__/todo.js
@@ -0,0 +1,54 @@
+const todoList = require('../todo');
+
+const {all, markAsComplete ,add, overdue, dueToday, dueLater } = todoList();
+
+describe("TodoList Test cases", () => {
+    beforeAll(() => {
+        const today = new Date();
+        const oneDay = 60*60*24*1000;
+        [
+            {
+                title: "Complete assignment",
+                completed: false,
+                dueDate: new Date(today.getTime() -1 * oneDay).toLocaleDateString("en-CA"
+                ),
+            },
+            {
+                title: "Go for shopping",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA"),
+            },
+            {
+                title: "Complete project",
+                completed: false,
+                dueDate: new Date(today.getTime() +1 * oneDay).toLocaleDateString("en-CA"),
+            
+            },
+        ].forEach(add);
+    });
+    test("Add new todo", () => {
+        expect(all.length).toEqual(3);
+        add(
+            {
+                title: "Take a test",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA"),
+            }
+        );
+        expect(all.length).toEqual(4);
+    });
+    test("Todo mark as complete", () => {
+        expect(all[0].completed).toEqual(false);
+        markAsComplete(0);
+        expect(all[0].completed).toEqual(true);
+    });
+    test("Test for overdue", () => {
+        expect(overdue().length).toEqual(1);
+    });
+    test("Test for today", () => {
+        expect(dueToday().length).toEqual(2);
+    });
+    test("Test for due later", () => {
+        expect(dueLater().length).toEqual(1);
+    });
+});
---
+++ .husky/todo.js
@@ -0,0 +1,29 @@
+const todoList = () => {
+    let all = [];
+    const add = (todoItem) => {
+      all.push(todoItem);
+    };
+    const markAsComplete = (index) => {
+      all[index].completed = true;
+    };
+  
+    const overdue = () => {
+    return all.filter((todo) => todo.dueDate < new Date().toLocaleDateString("en-CA")); 
+        
+    };
+  
+    const dueToday = () => {
+        return all.filter((todo) => todo.dueDate === new Date().toLocaleDateString("en-CA")); 
+    };
+
+  
+    const dueLater = () => {
+        return all.filter((todo) => todo.dueDate > new Date().toLocaleDateString("en-CA")); 
+      
+    };
+  
+  
+    return { all, add, markAsComplete, overdue, dueToday, dueLater };
+  };
+
+  module.exports = todoList;
